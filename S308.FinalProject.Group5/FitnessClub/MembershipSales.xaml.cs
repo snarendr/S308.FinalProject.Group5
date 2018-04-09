58e4abdafcbffef495f2d806a783f0716c08a0a5
snarendr <snarendr@indiana.edu>
2018-04-09 08:23:24 -0400

    working to save

--- S308.FinalProject.Group5/FitnessClub/MembershipSales.xaml.cs
+++ S308.FinalProject.Group5/FitnessClub/MembershipSales.xaml.cs
@@ -1,5 +1,5 @@
 ﻿//MSN Logo Source: https://www.apk4now.com/apk/1424/msn-health-amp-fitness-workouts
-//Price Quote Image Source: https://needtshirtsnow.com/T-shirt-Price-Quote
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
