ad712214ef9175fe8c9ae5fad57769b7aaf49c5f
njobst@indiana.edu <njobst@indiana.edu>
2018-04-26 18:14:43 -0400

    registration

--- S308.FinalProject.Group5/FitnessClub/MembershipRegistration.xaml.cs
+++ S308.FinalProject.Group5/FitnessClub/MembershipRegistration.xaml.cs
@@ -57,28 +57,34 @@ namespace FitnessClub
             strCCNum = txtCredCardNum.Text;
             strPhone = txtPhone.Text;
 
-
-
             //validate inputs
-            if (strFirstName == "" || strLastName == "" || strEmail == "" || strCCNum == "" || strPhone == "") ;
+            if (strFirstName == "" || strLastName == "" || strEmail == "" || strCCNum == "" || strPhone == "") 
             {
                 MessageBox.Show("Please fill out all information (First Name, Last Name, Email, Phone, Credit Card Number");
                 return;
             }
 
-            if (!int.TryParse(txtAge.Text, out intAge)) ;
+            //if the user provided an age, validate it is a valid number
+            if (txtAge.Text != "")
             {
-                MessageBox.Show("Please enter a number for Age");
-                return;
-            }
+                if (!int.TryParse(txtAge.Text, out intAge))
+                {
+                    MessageBox.Show("Please enter a number for Age");
+                    return;
+                }
 
-            if (!int.TryParse(txtWeight.Text, out intWeight)) ;
+            }
+            //if the user provided a weight, validate it is a number
+            if(txtAge.Text != "")
             {
-                MessageBox.Show("Please enter a number for Weight");
-                return;
+                if (!int.TryParse(txtWeight.Text, out intWeight))
+                {
+                    MessageBox.Show("Please enter a number for Weight");
+                    return;
+                }
             }
-
-            if (cmbCredCardType.SelectedIndex == -1) ;
+            //validate the user selected a credit card type
+            if (cmbCredCardType.SelectedIndex == -1) 
             {
                 MessageBox.Show("Please select a credit card type");
                 return;
@@ -90,17 +96,17 @@ namespace FitnessClub
                 MessageBox.Show("Please select a gender");
             }
 
-            if (rdbFemale.IsChecked == true) ;
+            if (rdbFemale.IsChecked == true) 
             {
                 strGender = rdbFemale.Content.ToString();
             }
 
-            if (rdbMale.IsChecked == true) ;
+            if (rdbMale.IsChecked == true) 
             {
                 strGender = rdbMale.Content.ToString();
             }
 
-            if (rdbNotProvided.IsChecked == true) ;
+            if (rdbNotProvided.IsChecked == true) 
             {
                 strGender = rdbNotProvided.Content.ToString();
             }
