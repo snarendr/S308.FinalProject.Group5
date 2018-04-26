b4c1557ef10b0967a9e0405c54ff92def1e815d9
Nate Jobst <njobst@indiana.edu>
2018-04-26 19:52:38 -0400

    membership registration

--- S308.FinalProject.Group5/FitnessClub/MembershipRegistration.xaml.cs
+++ S308.FinalProject.Group5/FitnessClub/MembershipRegistration.xaml.cs
@@ -57,11 +57,155 @@ namespace FitnessClub
             strCCNum = txtCredCardNum.Text.Trim();
             strPhone = txtPhone.Text.Trim();
 
-            //validate inputs
-            if (strFirstName == "" || strLastName == "" || strEmail == "" || strCCNum == "" || strPhone == "") 
+            //validate the user provided a first name
+            if (strFirstName == "")
             {
-                MessageBox.Show("Please fill out all information (First Name, Last Name, Email, Phone, Credit Card Number");
+                MessageBox.Show("Please provide a first name.");
                 return;
+            }
+
+            //validate the user provided a last name
+            if (strLastName == "")
+            {
+                MessageBox.Show("Please provide a last name.");
+                return;
+            }
+
+            //validate the user selected a credit card type
+            if (cmbCredCardType.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a credit card type");
+                return;
+
+            }
+
+            //validate the user provided a credit card number
+            if (strCCNum == "")
+            {
+                MessageBox.Show("Please provide a credit card number.");
+                return;
+            }
+
+            //validate the credit card is 15 or 16 digits
+            if (strCCNum.Length != 15 && strCCNum.Length != 16)
+            {
+                MessageBox.Show("Please enter a credit card number that is 15 or 16 digits.");
+                return;
+            }
+
+            //validate the credit card is a valid number
+                long lngCreditCard;
+            if (!long.TryParse(strCCNum, out lngCreditCard))
+            {
+                MessageBox.Show("Please enter only numeric digits for the credit card. Please do not enter any alphanumeric characters or non-numeric characters.");
+                return;
+            }
+
+            //validate the user provided a phone number
+            if (strPhone == "")
+            {
+                MessageBox.Show("Please provide a phone number.");
+                return;
+            }
+
+            //validate the phone number is 10 digits
+            if(strPhone.Length != 10)
+            {
+                MessageBox.Show("Please enter a 10 digit phone number.");
+                return;
+
+            }
+
+            //validate the phone number is all numeric
+            long lngPhoneNumber;
+            if (!long.TryParse(strPhone, out lngPhoneNumber))
+            {
+                MessageBox.Show("Please enter only numeric digits for the phone number. Please do not enter any alphanumeric characters or non-numeric characters.");
+                return;
+            }
+
+            //validate the user provided an email
+            if (strEmail == "")
+            {
+                MessageBox.Show("Please provide an email address.");
+                return;
+            }
+
+            //Validate the email does not contain any spaces
+            if (strEmail.Contains(" "))
+            {
+                MessageBox.Show("Please enter a valid email address that does contain spaces within the address.");
+                return;
+            }
+
+            //Validate that the email contains an @ symbol
+            if (!strEmail.Contains("@"))
+            {
+                MessageBox.Show("Please enter a valid email address that contains an @ symbol.");
+                return;
+            }
+            //Validate that the email contains a period
+            if (!strEmail.Contains("."))
+            {
+                MessageBox.Show("Please enter a valid email address that contains a '.' (period) symbol.");
+                return;
+            }
+
+            //Determine the index of the @ symbol and the period to test address valididity below
+            int intSymbol = strEmail.Trim().IndexOf("@");
+            int intPeriod = strEmail.Trim().IndexOf(".");
+
+            //Validate that the @ symbol appears before the period 
+            if (intSymbol - intPeriod > 0)
+            {
+                MessageBox.Show("Please enter a valid email address. The @ symbol must appear before the period.");
+                return;
+            }
+
+            //Validate that at least one character is provided before the @ symbol
+            if (intSymbol == 0)
+            {
+                MessageBox.Show("Please enter a valid email address. There must be at least one character before the @ symbol.");
+                return;
+            }
+
+            //Validate that at least one character is present between the @ symbol and the period
+            if (intPeriod - intSymbol == 1)
+            {
+                MessageBox.Show("Please enter a valid email address. There must be at least one character between the @ symbol and the period.");
+                return;
+            }
+
+            //Determine the length of the email for futher validation below
+            int intEmailLength = strEmail.Length;
+
+            //Subtract the email length and the index of the period to determine if at least two characters appear after the period in the email address. 
+            if (intEmailLength - intPeriod < 3)
+            {
+                MessageBox.Show("Please enter a valid email address. There must be at least two characters after the period.");
+                return;
+            }
+
+            //validate a gender was provided
+            if (rdbMale.IsChecked == false && rdbFemale.IsChecked == false && rdbNotProvided.IsChecked == false) ;
+            {
+                MessageBox.Show("Please select a gender");
+            }
+            
+            //store the selected gender type
+            if (rdbFemale.IsChecked == true)
+            {
+                strGender = rdbFemale.Content.ToString();
+            }
+
+            if (rdbMale.IsChecked == true)
+            {
+                strGender = rdbMale.Content.ToString();
+            }
+
+            if (rdbNotProvided.IsChecked == true)
+            {
+                strGender = rdbNotProvided.Content.ToString();
             }
 
             //if the user provided an age, validate it is a valid number
@@ -74,6 +218,7 @@ namespace FitnessClub
                 }
 
             }
+
             //if the user provided a weight, validate it is a number
             if(txtAge.Text != "")
             {
@@ -83,50 +228,11 @@ namespace FitnessClub
                     return;
                 }
             }
-            //validate the user selected a credit card type
-            if (cmbCredCardType.SelectedIndex == -1) 
-            {
-                MessageBox.Show("Please select a credit card type");
-                return;
+            
 
-            }
-            //validate the credit card is a number
-            long lngPhoneNumber;
-            if(!long.TryParse(strCCNum, out lngPhoneNumber))
-            {
-                MessageBox.Show("Please enter only numeric digits for the credit card. Please do not enter any alphanumeric characters or non-numeric characters.");
-                return;
-            }
+            
 
-            //insert futher email, phone, credit validation here
-
-            if(strCCNum.Length != 15 && strCCNum.Length != 16)
-            {
-                MessageBox.Show("Please enter a credit card number that is 15 or 16 digits.");
-                return;
-
-            }
-
-            if (rdbMale.IsChecked == false && rdbFemale.IsChecked == false && rdbNotProvided.IsChecked == false) ;
-            {
-                MessageBox.Show("Please select a gender");
-            }
-
-            if (rdbFemale.IsChecked == true) 
-            {
-                strGender = rdbFemale.Content.ToString();
-            }
-
-            if (rdbMale.IsChecked == true) 
-            {
-                strGender = rdbMale.Content.ToString();
-            }
-
-            if (rdbNotProvided.IsChecked == true) 
-            {
-                strGender = rdbNotProvided.Content.ToString();
-            }
-
+    
             //add results to membership database
 
             MembersInformation MemberNew = new MembersInformation(quote.MembershipType, strFirstName, strLastName, quote.StartDate.ToString(), quote.EndDate.ToString(), quote.SubTotal, quote.AdditionalFeatures_Training, quote.AdditionalFeatures_LockerRental, quote.TotalCost, strPhone, strEmail, strGender, intWeight);
